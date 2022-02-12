b83832cb9d651de6a8be0a5760a68833b15449d2
Sakshi Shelar <80898834+Sakshi-232@users.noreply.github.com>
2022-02-12 13:25:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Textform from './Components/Textform';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <div className="container">
+      <Textform heading="Learn About Historical Places"/>
+      </div>
+      </>
   );
 }
 
